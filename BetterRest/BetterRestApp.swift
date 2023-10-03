ac825ee65393bb29e49c784fc08ae2bbd26053be
IvanTilev <iivantilev@gmail.com>
2023-10-03 22:21:03 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ivan Tilev on 3.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
